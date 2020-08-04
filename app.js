05796f22ad9a6e17451f733117ddb3ab71a776ff
beata <beata@emc2wave.com>
2020-08-04 14:53:11 -0700

    loop articles

--- app.js
+++ app.js
@@ -4,8 +4,13 @@ const articles = document.querySelectorAll('.content');
 
 about.addEventListener('click', (e) => {
   const id = e.target.dataset.id;
+  // remove active from other buttons
   btns.forEach((btn) => {
     btn.classList.remove('active');
     e.target.classList.add('active');
+  });
+  // hide other articles
+  articles.forEach((article) => {
+    article.classList.remove('active');
   });
 });
